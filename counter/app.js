31d9f78179f047fd7d1864942d164278283d4e18
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-02-02 21:50:54 -0800

    Create app.js

---
+++ counter/app.js
@@ -0,0 +1,13 @@
+const increase = document.querySelector('.add');
+const decrease = document.querySelector('.minus');
+var total = 0;
+
+increase.addEventListener("click", function () {
+    total += 1;
+    document.querySelector(".amount").innerHTML = total;
+})
+
+decrease.addEventListener("click", function () {
+    total -= 1;
+    document.querySelector(".amount").innerHTML = total;
+})
