594fdc3d69a12695b4f0f7ce002942b4a12fc1eb
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-04-28 07:56:41 -0700

    Create Triangular Number Sequence.js

---
+++ algorithms/Triangular Number Sequence.js
@@ -0,0 +1,8 @@
+//Write a function that returns the number of dots when given its corresponding triangle number of the sequence.
+
+function triangle(n) {
+	return n * (n + 1) / 2;
+}
+
+//another way to write the same code
+const triangle = n => n * (n + 1) / 2;
